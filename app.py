f30abfde0165255e7f5261cf0779b51ed6bf56fe
Abdulwahaab710 <Abdulwahaab.ahmed710@gmail.com>
2016-11-18 10:41:20 -0500

    logs

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 @app.before_request
 def csrf_protect():
     if request.method == "POST":
